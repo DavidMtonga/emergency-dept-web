c8aeee8cab330ed3a9a6f6c5b373c6cd78700305
tamani phiri <tamanigabriel0@gmail.com>
2023-10-25 22:58:44 +0200

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <div className="top-0 fixed w-full bg-slate-950 text-white border-b border-slate-800">
       <nav className="flex w-full justify-between px-4 md:px-10 py-5">
-        <Link to={'/home'}>logo</Link>
+        <Link to={'/home'} className=" font-extrabold text-xl">Logo</Link>
         <div className="flex gap-4">
           <Link to={""} className="hover:text-[#00df9a] duration-300">link1</Link>
           <Link to={""} className="hover:text-[#00df9a] duration-300">link1</Link>
