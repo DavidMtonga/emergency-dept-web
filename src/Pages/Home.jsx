bbea99e99b50e36fa6cb7cf164f523c674b35916
tamani phiri <tamanigabriel0@gmail.com>
2023-10-25 23:03:43 +0200

    c

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,42 +1,43 @@
 import Typed from "react-typed";
 import Body from "../components/home/Body";
 
-
 const Home = () => {
   return (
-    <main className="min-h-screen bg-slate-950 text-white p-4 md:px-10 flex flex-col justify-center items-center py-24">
-      <div className="text-[#00df9a] flex-col w-full">
-        <h1 className=" md:text-5xl text-[#00df9a] sm:text-4xl text-xl font-bold">
-          EMERGENCY HELP DEPARTMENTS
-        </h1>
-        <Typed
-          className=" md:text-5xl text-[#00df9a] sm:text-4xl text-xl font-bold"
-          strings={["POLICE", "FIRE", "HEALTH"]}
-          typeSpeed={120}
-          backSpeed={140}
-          loop
-        />
-        <p className="">
-          The proposed mobile app for the fire department serves as a crucial
-          lifeline for our community, offering rapid and efficient access to
-          emergency services with the simple touch of a button. By seamlessly
-          connecting users to the emergency department, it empowers citizens to
-          swiftly summon assistance during critical situations, such as fires,
-          accidents, or medical emergencies, ensuring timely responses that can
-          be the difference between life and death.
-        </p>
+    <main className="bg-slate-950 text-white p-4 md:px-10 flex flex-col justify-center items-center py-24">
+      <div className="text-[#00df9a] min-h-screen items-center justify-center grid lg:grid-cols-2 grid-cols-1 w-full">
+        <div>
+          <h1 className=" md:text-5xl text-[#00df9a] sm:text-4xl text-xl font-bold">
+            EMERGENCY <span className="text-white">HELP DEPARTMENTS</span>
+          </h1>
+          <Typed
+            className=" md:text-5xl text-[#00df9a] sm:text-4xl text-xl font-bold"
+            strings={["POLICE", "FIRE", "HEALTH"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+          <p className="">
+            The proposed mobile app for the fire department serves as a crucial
+            lifeline for our community, offering rapid and efficient access to
+            emergency services with the simple touch of a button. By seamlessly
+            connecting users to the emergency department, it empowers citizens
+            to swiftly summon assistance during critical situations, such as
+            fires, accidents, or medical emergencies, ensuring timely responses
+            that can be the difference between life and death.
+          </p>
+        </div>
+        <div>
+          <p>
+            Moreover, the app&#39;s integration of location services ensures
+            that responders can pinpoint the caller&#39;s exact position,
+            further expediting their arrival and potentially reducing response
+            times. In this way, this app not only promotes community safety but
+            also instills a sense of security, knowing that help is readily
+            available when needed most.
+          </p>
+        </div>
       </div>
-      <div className="">
-        <p>
-          Moreover, the app&#39;s integration of location services ensures that
-          responders can pinpoint the caller&#39;s exact position, further
-          expediting their arrival and potentially reducing response times. In
-          this way, this app not only promotes community safety but also
-          instills a sense of security, knowing that help is readily available
-          when needed most.
-        </p>
-      </div>
-      <Body/>
+      <Body />
     </main>
   );
 };
