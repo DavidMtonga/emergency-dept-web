243c923d57b506650520a69478f39d750bc8cf1f
tamani phiri <tamanigabriel0@gmail.com>
2023-10-25 21:37:32 +0200

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div className="top-0 fixed w-full bg-gray-800">
-      <nav className="flex w-full justify-between px-4 md:px-10">
+    <div className="top-0 fixed w-full bg-slate-950 text-white">
+      <nav className="flex w-full justify-between px-4 md:px-10 py-5">
         <div>logo</div>
-        <div>links</div>
+        <div className="flex gap-4">
+          <Link>link1</Link>
+          <Link>link1</Link>
+          <Link>link1</Link>
+          <Link>link1</Link>
+        </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
