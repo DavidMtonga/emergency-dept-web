c7f45446d1a10af05170b1bd0127c7aa485fc9d7
tamani phiri <tamanigabriel0@gmail.com>
2023-10-26 09:12:50 +0200

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,12 @@ const Navbar = () => {
   return (
     <div className="top-0 fixed z-50 w-full bg-opacity-80 backdrop-blur bg-slate-950 text-white border-b border-slate-800">
       <nav className="flex w-full justify-between relative px-4 md:px-10 py-5">
+        {/* Logo */}
         <Link to={"/home"} className=" font-extrabold text-xl">
           Logo
         </Link>
+
+        {/* Desk */}
         <div className="lg:flex gap-4 hidden">
           <Link to={""} className="hover:text-[#00df9a] duration-300">
             Departments
@@ -23,6 +26,8 @@ const Navbar = () => {
             link1
           </Link>
         </div>
+
+        {/* Mobile menu */}
         <div className="lg:hidden flex">
           <button
             onClick={() => setOpen(!open)}
