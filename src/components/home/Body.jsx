0a0b5d649bafbf059fb429bab9e33e0ef39d1b79
tamani phiri <tamanigabriel0@gmail.com>
2023-10-26 08:17:34 +0200

    c

--- src/components/home/Body.jsx
+++ src/components/home/Body.jsx
@@ -3,7 +3,8 @@ import { departments } from "../../constants/departments";
 
 const Body = () => {
   return (
-    <div className="mb-4 w-full flex justify-evenly gap-4 md:flex-row flex-col min-h-screen items-center p-4 md:px-10">
+    <div className="min-h-screen grid items-center justify-center w-full">
+    <div className="mb-4 w-full flex justify-evenly gap-4 md:flex-row flex-col items-center p-4 md:px-10">
       {departments.map((item, i) => (
         <div
           key={i}
@@ -21,6 +22,7 @@ const Body = () => {
           <Link to={item.link} className=" bg-slate-950 text-white hover:text-slate-950 hover:bg-[#00df9a] px-4 py-2 duration-300 shadow-md rounded-full">Select Department</Link>
         </div>
       ))}
+    </div>
     </div>
   );
 };
