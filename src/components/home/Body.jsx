6d970776e95ab7156ed351e584a4db28f5715b63
tamani phiri <tamanigabriel0@gmail.com>
2023-10-26 08:18:44 +0200

    c

--- src/components/home/Body.jsx
+++ src/components/home/Body.jsx
@@ -3,26 +3,40 @@ import { departments } from "../../constants/departments";
 
 const Body = () => {
   return (
-    <div className="min-h-screen grid items-center justify-center w-full">
-    <div className="mb-4 w-full flex justify-evenly gap-4 md:flex-row flex-col items-center p-4 md:px-10">
-      {departments.map((item, i) => (
-        <div
-          key={i}
-          className=" flex bg-white flex-col text-center justify-center gap-4 items-center md:w-fit w-full rounded-lg border p-4 shadow-md  "
-        >
-          <div className=" items-center justify-center w-full flex">
-            <img
-              className="rounded-full h-20"
-              src={item.image}
-              alt="police department"
-              loading="lazy"
-            />
+    <div className="min-h-screen grid items-center w-full">
+      <div>
+        <h1>Departments</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
+          voluptatem id sed consectetur eius labore amet harum, vitae aperiam
+          hic saepe eveniet cumque nihil necessitatibus earum error, distinctio
+          exercitationem. Quibusdam.
+        </p>
+      </div>
+      <div className="mb-4 w-full flex justify-evenly gap-4 md:flex-row flex-col items-center p-4 md:px-10">
+        {departments.map((item, i) => (
+          <div
+            key={i}
+            className=" flex bg-white flex-col text-center justify-center gap-4 items-center md:w-fit w-full rounded-lg border p-4 shadow-md  "
+          >
+            <div className=" items-center justify-center w-full flex">
+              <img
+                className="rounded-full h-20"
+                src={item.image}
+                alt="police department"
+                loading="lazy"
+              />
+            </div>
+            <h1 className="text-xl text-gray-500">{item.name}</h1>
+            <Link
+              to={item.link}
+              className=" bg-slate-950 text-white hover:text-slate-950 hover:bg-[#00df9a] px-4 py-2 duration-300 shadow-md rounded-full"
+            >
+              Select Department
+            </Link>
           </div>
-          <h1 className="text-xl text-gray-500">{item.name}</h1>
-          <Link to={item.link} className=" bg-slate-950 text-white hover:text-slate-950 hover:bg-[#00df9a] px-4 py-2 duration-300 shadow-md rounded-full">Select Department</Link>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
