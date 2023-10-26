a4fe1cd4d9585b5eb979f843243059fef59e31d3
tamani phiri <tamanigabriel0@gmail.com>
2023-10-26 09:01:40 +0200

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,11 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="lg:hidden flex">
-          <button onClick={() => setOpen(!open)}>menu</button>
+          <button onClick={() => setOpen(!open)} className="flex flex-col gap-3">
+            <div className="h-1 w-8 bg-white"></div>
+            <div className="h-1 w-8 bg-white"></div>
+            <div className="h-1 w-8 bg-white"></div>
+          </button>
           <div className=" h-screen flex-col flex p-4 pt-6 gap-4 bg-slate-950 w-2/3 absolute top-[100%] right-0">
             <Link to={""} className="hover:text-[#00df9a] duration-300">
               Departments
