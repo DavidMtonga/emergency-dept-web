3b57d0a5fbbe03a966a2b3f92c7bbcde2f4a0745
tamani phiri <tamanigabriel0@gmail.com>
2023-10-26 09:11:26 +0200

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
-  const[open,setOpen]=useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <div className="top-0 fixed z-50 w-full bg-opacity-80 backdrop-blur bg-slate-950 text-white border-b border-slate-800">
       <nav className="flex w-full justify-between relative px-4 md:px-10 py-5">
@@ -24,11 +24,18 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="lg:hidden flex">
-          <button onClick={() => setOpen(!open)} className="flex flex-col gap-2">
+          <button
+            onClick={() => setOpen(!open)}
+            className="flex flex-col gap-2"
+          >
             <div className="h-1 w-8 rounded-full bg-white"></div>
             <div className="h-1 w-8 rounded-full bg-white"></div>
           </button>
-          <motion.div animate={open?{translateX:0}:{translateX:"100%"}} transition={{type:"tween"}} className=" h-screen flex-col flex p-4 pt-6 gap-4 bg-slate-950 w-2/3 absolute top-[100%] right-0">
+          <motion.div
+            animate={open ? { translateX: 0 } : { translateX: "100%" }}
+            transition={{ type: "tween" }}
+            className=" h-screen flex-col flex p-4 pt-6 gap-4 bg-slate-950 w-2/3 absolute top-[100%] right-0"
+          >
             <Link to={""} className="hover:text-[#00df9a] duration-300">
               Departments
             </Link>
@@ -41,7 +48,12 @@ const Navbar = () => {
             <Link to={""} className="hover:text-[#00df9a] duration-300">
               link1
             </Link>
-            <Link to={""} className="bg-[#00df9a] active:scale-95 duration-300 text-slate-950 w-fit px-4 py-2 rounded-full">Login</Link>
+            <Link
+              to={""}
+              className="bg-[#00df9a] active:scale-95 duration-300 text-slate-950 w-fit px-4 py-2 rounded-full"
+            >
+              Login
+            </Link>
           </motion.div>
         </div>
       </nav>
